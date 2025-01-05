2bae2beb6f5520a5af6439fb2a69548552bd0deb
Ivaz <ivazreza@gmail.com>
2025-01-05 23:45:27 +0700

    Finishing the style of markdown app

--- markdownEditorApp/src/components/main.jsx
+++ markdownEditorApp/src/components/main.jsx
@@ -7,17 +7,57 @@ const Main = () => {
   const dispatch = useDispatch();
 
   return (
-    <main>
-      <textarea
-        value={rawInput}
-        onChange={(e) => dispatch(changeInput(e.target.value))}
-        name="input"
-        id="editor"
-      ></textarea>
-      <div
-        id="preview"
-        dangerouslySetInnerHTML={{ __html: preview }}
-      ></div>
+    <main className='
+      d-flex
+      flex-column
+      justify-content-start
+      align-items-center
+      pt-4
+      gap-3
+    '>
+      <section className='editor'>
+        <div className='
+          sectionHead
+          d-flex
+          flex-row
+          gap-1
+          p-2
+          align-items-center
+        '>
+          <i className="fa-solid fa-keyboard"></i>
+          <h3>Editor</h3>        
+        </div>
+        <textarea
+          className='sectionBody p-2'
+          value={rawInput}
+          onChange={(e) => dispatch(changeInput(e.target.value))}
+          name="input"
+          id="editor"
+          rows="5"
+          cols="50"
+        ></textarea>
+      </section>
+      <section className='previewer'>
+        <div className='
+          sectionHead
+          d-flex
+          flex-row
+          gap-1
+          p-2
+          align-items-center
+        '>
+          <i className="fa-solid fa-eye"></i>
+          <h3>Previewer</h3>
+        </div>
+        <div
+          className='sectionBody p-2'
+          id="preview"
+          style={{
+            width: "30rem"
+          }}
+          dangerouslySetInnerHTML={{ __html: preview }}
+        ></div>
+      </section>
     </main>
   );
 };
