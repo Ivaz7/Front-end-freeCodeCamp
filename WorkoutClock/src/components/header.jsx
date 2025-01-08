15c013b88dad3ce3025d61b1a0b81f5dce1de573
Ivaz <ivazreza@gmail.com>
2025-01-08 11:03:00 +0700

    Make header for workout

---
+++ WorkoutClock/src/components/header.jsx
@@ -0,0 +1,15 @@
+const Header = () => {
+  return (
+    <header 
+      className="
+        d-flex
+        justify-content-center
+        align-items-end
+      "
+    >
+      <h1>Workout Clock</h1>
+    </header>
+  )
+}
+
+export default Header;
