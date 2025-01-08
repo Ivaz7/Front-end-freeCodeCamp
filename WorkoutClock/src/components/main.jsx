99254f843f621013faf045949bdbdc975eb3f9f8
Ivaz <ivazreza@gmail.com>
2025-01-08 11:24:50 +0700

    Add Time render to render session/break time

--- WorkoutClock/src/components/main.jsx
+++ WorkoutClock/src/components/main.jsx
@@ -1,9 +1,18 @@
 import LengthBreakSession from "./lengthBrkSion";
+import TimeRender from "./timeRender";
 
 const Main = () => {
   return (
-    <main>
+    <main
+      className="
+        d-flex
+        flex-column
+        gap-4
+        align-items-center
+      "  
+    >
       <LengthBreakSession />
+      <TimeRender />
     </main>
   )
 }
---
+++ WorkoutClock/src/components/timeRender.jsx
@@ -0,0 +1,18 @@
+const TimeRender = () => {
+  return (
+    <section
+      className="
+        timeRender
+        d-flex
+        flex-column
+        align-items-center
+        gap-2
+      "
+    >
+      <h4>Session Time</h4>
+      <h1>25:00</h1>
+    </section>
+  )
+}
+
+export default TimeRender;
